def77261fd29314a17d37ce99bcb0b99e19a1b8e
vincentliuk <vincentliuk@gmail.com>
2015-07-05 20:22:19 -0400

    second

--- RNN/LSTMLayer.h
+++ RNN/LSTMLayer.h
@@ -1,43 +1,21 @@
-/* 
- * File:   LSTMLayer.h
- * Author: kai
- *
- * Created on July 1, 2015, 4:04 PM
- */
-
-#ifndef LSTMLAYER_H
-#define	LSTMLAYER_H
-
+#pragma once
 #include "../BaseLayer/BaseLayer.h"
 
-namespace NeuralNet{
+namespace NeuralNet {
 
-struct LSTMBrick: public BaseLayer{
-		
-	
-	std::vector<LSTMBrick> inputSet;
-	
-}
+    class RNN_LSTM {
+    public:
+        forward();
+        backward();
+    private:
+        std::vector<BaseLayer> inGateLayers, forgetGateLayers, cellStateLayers, outputGateLayers, informationLayers;
+        std::vector<ElementWiseLayer> outputLayers, forgetElementGateLayers, inputElementGateLayers;
 
-}
+        int numHiddenLayers, hiddenLayerInputDim, hiddenLayerOutputDim;
+        int inputDim, outputDim;
 
 
-class RNN_LSTM{
-
-
-	
-
-	forward();
-	backward();
-	
-	
-	std::vector<BaseLayer> inGateLayers, forgetGateLayers, cellStateLayers, outputGateLayers, informationLayers;
-	std::vector<ElementWiseLayer>  outputLayers, forgetElementGateLayers, inputElementGateLayers;
-	int numHiddenLayers;
-	int inputDim, outputDim;
+    }
 
 }
-
-
-#endif	/* LSTMLAYER_H */
 
